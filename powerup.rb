489dafd0ae353e253a9df0f75a13d84a1c3daa37
Doug Stewart <doug@dougstewart.org>
2012-02-03 21:20:40 -0700

    Initial attempt at a Rails 3 application template. VERY FEW opinions asserted.

---
+++ powerup.rb
@@ -0,0 +1,17 @@
+# rails new appname -m powerup.rb
+
+# powerup.rb
+run "rm public/index.html"
+run "rm app/assets/images/rails.png"
+inject_into_file 'config/application.rb', :before => "  end" do
+  "\n    config.time_zone = 'Mountain Time (US & Canada)'\n\n"
+end
+
+git :init
+git :add => "."
+git :commit => "-a -m 'Initial commit, via template generator'"
+
+# COULD BE COOL?:
+# => Zurb Foundation gem by default?
+# => adding .erb to the end of generated .scss files
+# => a billion cats
