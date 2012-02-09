35d9dcc998bd6efc10a0af5b5f2efb6e74adecec
Fletcher Nichol <fletcher.nichol@ama.ab.ca>
2012-02-09 09:42:36 -0700

    Add after_bundler support which delays git commit until after bundle.

--- powerup.rb
+++ powerup.rb
@@ -1,4 +1,8 @@
-# rails new appname -m https://raw.github.com/amaabca/apptemplate/master/powerup.rb
+# rails new appname --skip-bundle -T -m https://raw.github.com/amaabca/apptemplate/master/powerup.rb
+
+def say_wizard(tag = "wizard", text); say "\033[1m\033[36m" + tag.to_s.rjust(10) + "\033[0m" + "  #{text}" end
+@after_bundler_blocks = []
+def after_bundler(&block); @after_bundler_blocks << block; end
 
 # powerup.rb
 run "rm public/index.html"
@@ -7,9 +11,16 @@ inject_into_file 'config/application.rb', :before => "  end" do
   "\n    config.time_zone = 'Mountain Time (US & Canada)'\n\n"
 end
 
-git :init
-git :add => "."
-git :commit => "-a -m 'Initial commit, via template generator'"
+after_bundler do
+  git :init
+  git :add => "."
+  git :commit => "-a -m 'Initial commit, via template generator'"
+end
+
+say_wizard "Running Bundler install. This will take a while."
+run 'bundle install'
+say_wizard "Running after Bundler callbacks."
+@after_bundler_blocks.each { |blk| blk.call }
 
 # COULD BE COOL?:
 # => Zurb Foundation gem by default?
